2a2444cc2c51c02cae5b7bccbc368a38eba8f6af
x23125721 <x23125721@student.ncirl.ie>
2024-04-24 09:46:52 +0100

    new code commitss

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -64,25 +64,25 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'petshop1',
-#         'USER':'postgres',
-#         'PASSWORD':'password',
-#         'HOST':'x23125721-petshop-cpp-db.civwddw9gdry.eu-north-1.rds.amazonaws.com',
-#         'PORT':'5432'
-
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'petshop1',
+        'USER':'postgres',
+        'PASSWORD':'password',
+        'HOST':'x23125721-petshop-cpp-db.civwddw9gdry.eu-north-1.rds.amazonaws.com',
+        'PORT':'5432'
+
+    }
+}
 
 
 
